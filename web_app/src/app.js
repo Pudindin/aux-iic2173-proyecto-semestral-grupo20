243ad8c2a5bf21aeebe21d5e2e0f42f79d470a31
Diego Vásquez <dhvasquez@uc.cl>
2020-10-10 14:16:05 -0300

    apply instance header information

--- web_app/src/app.js
+++ web_app/src/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const request = require('request');
 const routes = require('./routes');
 
 // Constructor
@@ -13,11 +14,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setting views
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.json());
 
 // Setting statics
 app.use('/', express.static(path.join(__dirname, 'public')));
+
+// Setting a header
+app.use((req, res, next) => {
+  request('http://169.254.269.254/latest/meta-data/instance-id', (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      res.setHeader('EC2-instace-id', body);
+    }
+  });
+  next();
+});
 
 // Setting routes
 app.use('', routes);
