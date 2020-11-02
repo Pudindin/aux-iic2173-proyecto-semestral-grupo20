013342d7137c453e084ea712ebbc391452d6ed9c
Pudindin <dncaroca@uc.cl>
2020-11-02 11:32:33 -0300

    scripts invocan servicio

--- web_app/scripts/start.sh
+++ web_app/scripts/start.sh
@@ -1,1 +1,1 @@
-docker-compose -f iic2173-proyecto-semestral-grupo20/web_app/docker-compose.yml up -d
+sudo systemctl start docker-compose-app
--- web_app/scripts/stop.sh
+++ web_app/scripts/stop.sh
@@ -1,2 +1,1 @@
-docker-compose -f iic2173-proyecto-semestral-grupo20/web_app/docker-compose.yml down
-docker stop $(docker ps -a -q)
+sudo systemctl stop docker-compose-app
