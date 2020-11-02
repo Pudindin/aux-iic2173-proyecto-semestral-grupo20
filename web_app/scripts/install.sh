d168151eef4c0fdae3bbb95932e7c1a7f639c8e4
Pudindin <dncaroca@uc.cl>
2020-11-02 14:01:54 -0300

    instalado pip3 en la máquina

--- web_app/scripts/install.sh
+++ web_app/scripts/install.sh
@@ -1,5 +1,3 @@
-sudo install python3.6
-sudo install python3-pip
 pip3 install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
 pwd=$(aws ecr get-login-password --region us-east-2)
