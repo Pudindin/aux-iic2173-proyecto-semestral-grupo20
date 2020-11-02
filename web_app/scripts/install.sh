af13d65c2e0fe585e4c207021a3e4398d253e8bb
Pudindin <dncaroca@uc.cl>
2020-11-02 08:24:20 -0300

    borro flag no incluir email

--- web_app/scripts/install.sh
+++ web_app/scripts/install.sh
@@ -2,7 +2,7 @@ install python3.6
 install python3-pip
 pip3 install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
-pwd=$(aws ecr get-login-password --region us-east-2 --no-include-email)
+pwd=$(aws ecr get-login-password --region us-east-2)
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd https://161618437221.dkr.ecr.us-east-2.amazonaws.com
 docker pull 161618437221.dkr.ecr.us-east-2.amazonaws.com/ass_g20
