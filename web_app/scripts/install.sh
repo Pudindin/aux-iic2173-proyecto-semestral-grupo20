5cb1e26d31f118cc03a6a34e8517637616ed2906
Pudindin <dncaroca@uc.cl>
2020-11-02 13:42:05 -0300

    vuelvo a instalar python y pip en install.sh

--- web_app/scripts/install.sh
+++ web_app/scripts/install.sh
@@ -1,3 +1,5 @@
+sudo install python3.6
+sudo install python3-pip
 pip3 install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
 pwd=$(aws ecr get-login-password --region us-east-2)
