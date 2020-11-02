7d47eaf277e946e78c4a6aaae398dad3ccee9568
Pudindin <dncaroca@uc.cl>
2020-11-02 00:12:01 -0300

    imprimir cosas

--- web_app/scripts/install.sh
+++ web_app/scripts/install.sh
@@ -1,4 +1,7 @@
-pwd=$(aws ecr get-login-password --region us-east-2)
+which aws
+aws --version
+pwd=$(aws ecr get-login-password --region us-east-2 --no-include-email)
+$pwd
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd https://161618437221.dkr.ecr.us-east-2.amazonaws.com
 docker pull 161618437221.dkr.ecr.us-east-2.amazonaws.com/ass_g20
