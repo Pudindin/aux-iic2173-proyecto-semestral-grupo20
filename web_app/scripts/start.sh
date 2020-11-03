14bbcd7c93440f1cae34e11f2a2348ade670e953
Pudindin <dncaroca@uc.cl>
2020-11-03 10:15:02 -0300

    sigo jugando son start.sh x2

--- web_app/scripts/start.sh
+++ web_app/scripts/start.sh
@@ -1,7 +1,10 @@
-echo $PWD
-la
-cd ..
-la
-cd ..
-la
+mv ../docker-compose.yml /home/ubuntu/
+mv ../src /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/
+mv ../package.json /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/
+mv ../package-lock.json /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/
+mv ../node-entrypoint.sh /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/
+mv ../index.js /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/
+mv ../nginx-conf /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/
+mv ../yarn.lock /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/
+mv ../node_modules /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/
 sudo systemctl start docker-compose-app
