04d11e19381388cb9ca8d89e3e7d33832e184da8
Pudindin <dncaroca@uc.cl>
2020-11-03 11:12:18 -0300

    muevo rm

--- web_app/scripts/install.sh
+++ web_app/scripts/install.sh
@@ -1,3 +1,14 @@
+sudo rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/pg-init-scripts
+sudo rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/nginx-conf
+sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/yarn.lock
+sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/package-lock.json
+sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/package.json
+sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/node-entrypoint.sh
+sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/index.js
+sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/Dockerfile
+sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/readme.md
+sudo rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/src
+sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/docker-compose.yml
 pip3 install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
 pwd=$(aws ecr get-login-password --region us-east-2)
--- web_app/scripts/stop.sh
+++ web_app/scripts/stop.sh
@@ -1,12 +1,1 @@
 sudo systemctl stop docker-compose-app
-rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/pg-init-scripts
-rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/nginx-conf
-rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/yarn.lock
-rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/package-lock.json
-rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/package.json
-rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/node-entrypoint.sh
-rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/index.js
-rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/Dockerfile
-rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/readme.md
-rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/ src
-rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/ docker-compose.yml 
