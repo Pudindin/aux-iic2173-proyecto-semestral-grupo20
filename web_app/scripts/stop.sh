3ca609dce4feb4884ae6ccb25dcebe325bbd3c94
Pudindin <dncaroca@uc.cl>
2020-11-03 12:33:40 -0300

    intento que borre archivos

--- web_app/scripts/stop.sh
+++ web_app/scripts/stop.sh
@@ -1,13 +1,1 @@
 sudo systemctl stop docker-compose-app
-sudo rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/pg-init-scripts
-sudo rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/nginx-conf
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/yarn.lock
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/package-lock.json
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/package.json
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/node-entrypoint.sh
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/index.js
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/Dockerfile
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/readme.md
-sudo rm -r /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/src
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/docker-compose.yml
-sudo rm /home/ubuntu/iic2173-proyecto-semestral-grupo20/web_app/docker-compose.production.yml
